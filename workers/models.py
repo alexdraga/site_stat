f43b7eed55e3c5a42ac818e531d8f8c1cd1601d9
Aleksandr Draga <5408879@gmail.com>
2017-08-10 20:15:25 +0300

    Fix wrong model adding

--- workers/models.py
+++ workers/models.py
@@ -28,28 +28,3 @@ class Worker(models.Model):
 
     def __str__(self):
         return self.worker_name
-
-
-class WorkerActivity(models.Model):
-
-    class WorkerNames():
-        GRAB = "grab"
-        ZIP = "zip"
-        REPORT = "report"
-        STATUSES = ((GRAB, "Grab worker"),
-                    (ZIP, 'Zip worker'),
-                    (REPORT, "Report worker"))
-    worker_name = models.CharField(verbose_name=_(u'Worker name'),
-                                   max_length=20,
-                                   choices=WorkerNames.STATUSES,
-                                   unique=True)
-    timeout = models.IntegerField(verbose_name=_(u'Worker timeout, s'), default=5)
-    next_run = models.DateTimeField(verbose_name=_(u'Next run'))
-    heartbeat = models.DateTimeField(verbose_name=_(u'Heartbeat'))
-
-    class Meta:
-        verbose_name = _('Worker')
-        verbose_name_plural = _("Workers")
-
-    def __str__(self):
-        return self.worker_name
