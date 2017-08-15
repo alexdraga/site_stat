30f7bf35fe84ae86d57b41730546828240f5091c
Aleksandr Draga <a_draga@wargaming.net>
2017-08-15 10:38:23 +0300

    Fix migrate_extension script

--- reports/management/commands/migrate_extension.py
+++ reports/management/commands/migrate_extension.py
@@ -3,7 +3,7 @@ from os import path, rename
 
 from reports.models import GrabberLog
 
-from site_stat.settings import GRABS_DIR
+from django.conf import settings
 
 
 NEW_EXTENSION = ".txt"
@@ -15,11 +15,12 @@ class Command(BaseCommand):
         not_affected = 0
         for grab in GrabberLog.objects.all():
             grab_filename = grab.filename.name
-            actual_filename = path.join(GRABS_DIR, grab_filename)
+            actual_filename = path.join(settings.GRABS_DIR, grab_filename)
             if path.exists(actual_filename) and path.isfile(actual_filename):
-                new_filename = self.change_extension(actual_filename, NEW_EXTENSION)
+                new_actual_filename = self.change_extension_actual(actual_filename, NEW_EXTENSION)
+                new_filename = self.change_extension_base(grab_filename, NEW_EXTENSION)
                 if actual_filename != new_filename:
-                    rename(actual_filename, new_filename)
+                    rename(actual_filename, new_actual_filename)
                     grab.filename = new_filename
                     grab.save()
                     renamed += 1
@@ -28,8 +29,15 @@ class Command(BaseCommand):
 
         print "Renamed %s records to %s extension, not affected: %s" % (renamed, NEW_EXTENSION, not_affected)
 
-    def change_extension(self, filename, extension):
+    def change_extension_actual(self, filename, extension):
         filepath, fileextension = path.splitext(filename)
+        if len(fileextension):
+            return filepath + extension
+        else:
+            return filepath
+
+    def change_extension_base(self, grab_filename, extension):
+        filepath, fileextension = path.splitext(grab_filename)
         if len(fileextension):
             return filepath + extension
         else:
